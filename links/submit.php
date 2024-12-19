cb248848ee39f48e61f849de845a66c851bd772c
INAUTUM <govnastii@gmail.com>
2024-12-20 01:26:33 +0300

    f

---
+++ links/submit.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "std-mysql.ist.mospolytech.ru:2306"; // адрес сервера
+$username = "std_2319_site"; // имя пользователя
+$password = "23289192"; // пароль
+$dbname = "std_2319_site"; // имя базы данных
+
+// Создание соединения
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка соединения
+if ($conn->connect_error) {
+    die("Ошибка подключения: " . $conn->connect_error);
+}
+
+// Получение данных из формы
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+// Подготовка SQL-запроса для вставки данных
+$stmt = $conn->prepare("INSERT INTO plants (title, description) VALUES (?, ?)");
+$stmt->bind_param("ss", $title, $description);
+
+if ($stmt->execute()) {
+    echo "Новая запись успешно добавлена";
+} else {
+    echo "Ошибка: " . $stmt->error;
+}
+
+// Закрытие соединения
+$stmt->close();
+$conn->close();
+?>
